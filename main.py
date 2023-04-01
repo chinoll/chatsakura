d402e0b177f271f7c77b69986326072897dac1c4
chinoll <chinoll@chinoll.org>
2023-04-01 23:10:38 +0800

    set max_length=2048

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ with gr.Blocks() as demo:
     def user(user_message, history,topk,temp,topp):
         # print(user_message,history)
         tokens = tokenizer.encode(f'{prompt}<human request>:{user_message}\n<bot response>:',return_tensors="pt").to(device)
-        outputs = model.generate(tokens,top_k=topk,top_p=topp,do_sample=True,temperature=temp)
+        outputs = model.generate(tokens,top_k=topk,top_p=topp,do_sample=True,temperature=temp,max_length=2048)
         bot_message = tokenizer.decode(outputs[0],skip_special_tokens=True)[len(tokens):]
         return "", history + [[user_message, bot_message]]
 
